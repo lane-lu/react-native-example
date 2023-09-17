cd98b8af5fea14c693edceb39ebad1858109e939
Lane Lu <llu@interprosoft.com>
2023-09-17 12:26:19 +0800

    fea: complete setting server url

--- App.tsx
+++ App.tsx
@@ -6,113 +6,89 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { Button, Text, TextInput } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+import PropertyEditor from 'react-native-property-editor';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+type RootStackParamList = {
+  'Home': undefined;
+  'Set Server URL': undefined;
+  'Get Server URL': undefined;
+};
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+type Props = NativeStackScreenProps<RootStackParamList>;
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Welcome' }}
+        />
+        <Stack.Screen name="Set Server URL" component={SetPropertyScreen} />
+        <Stack.Screen name="Get Server URL" component={GetPropertyScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+const HomeScreen = ({ navigation }: Props) => {
+  return (
+    <>
+      <Button
+        title="Set Server URL"
+        onPress={() => navigation.navigate('Set Server URL')}
+      />
+      <Button
+        title="Get Server URL"
+        onPress={() => navigation.navigate('Get Server URL')}
+      />
+    </>
+  );
+};
+
+const SetPropertyScreen = () => {
+  const [serverURL, setServerURL] = React.useState<string | undefined>();
+
+  React.useEffect(() => {
+    PropertyEditor.getString('x-server-url', 'http://www.google.com/').then(setServerURL);
+  }, []);
+
+  return (
+    <>
+      <Text>Input Server URL:</Text>
+      <TextInput
+        placeholder="Type server URL here"
+        onChangeText={(text) => setServerURL(text)}
+        defaultValue={serverURL}
+      />
+      <Button
+        title="Save"
+        onPress={() => PropertyEditor.setString('x-server-url', serverURL!)}
+      />
+    </>
+  )
+}
+
+const GetPropertyScreen = () => {
+  const [serverURL, setServerURL] = React.useState<string | undefined>();
+
+  React.useEffect(() => {
+    PropertyEditor.getString('x-server-url', 'http://www.google.com/').then(setServerURL);
+  }, []);
+
+  return (
+    <>
+      <Text>Server URL:</Text>
+      <Text>{serverURL}</Text>
+    </>
+  );
+}
 
 export default App;
